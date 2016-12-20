dec809ef95335014131a2d44cc878e2fddd8ed7b
Kuster Tyler (AA/PJ-COSE3-NA) <Tyler.Kuster@us.bosch.com>
2016-12-20 14:21:37 -0500

    added project

---
+++ FoilVibes/MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Tyler Kuster on 12/20/16.
+//  Copyright © 2016 Tyler Kuster. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
